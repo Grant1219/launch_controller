9fa4c7de44ddb76a8ebac34a70725e2642ebac62
Grant <grant@tuxinator.net>
2021-04-20 22:02:17 -0700

    Add launch controller logic
    My current idea is to have the microcontroller sleep until inputs
    change. After the "arm" switch is triggered the interrupt will fire and
    the controller will poll for the "launch" button until it is pressed for
    one second, or the inputs change and it goes back to sleep.
    
    The controller will need to be power cycled after each launch, which is
    an effort to prevent accidental launch triggers.

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+volatile uint8_t launch_armed = 0;
+volatile uint8_t key_switch = 0;
+
+ISR(PCINT0_vect) {
+    // this interrupt fires whenever input is received on a pin
+    key_switch = PINB & _BV(PB3);
+
+    if (key_switch) {
+        launch_armed = 1;
+        // TODO enable "armed" LED
+    }
+}
 
 int main(void) {
     // set all ports to output mode
@@ -9,14 +24,61 @@ int main(void) {
     // set output of all ports to zero
     PORTB = 0x00;
 
+    // configure pins 3 and 4 as input
+    DDRB &= ~(_BV(PB3));
+    DDRB &= ~(_BV(PB4));
+    // activate pull-up resistors on pins 3 and 4
+    PORTB |= _BV(PB3);
+    PORTB |= _BV(PB4);
+
     while (1) {
-        // turn pin 0 on
-        //PORTB |= _BV(PB0);
-        PORTB = 0xff;
-        _delay_ms(500);
-        // turn pin 0 off
-        //PORTB &= ~(_BV(PB0));
-        PORTB = 0x00;
-        _delay_ms(500);
+        // disable interrupts
+        cli();
+
+        if (launch_armed) {
+            key_switch = 0;
+            uint8_t ignite_button = 0;
+
+            // check for ignite button
+            key_switch = PINB & _BV(PB3);
+            ignite_button = PINB & _BV(PB4);
+
+            if (ignite_button) {
+                // delay for 1 second and check if all inputs are still set
+                _delay_ms(1000);
+                key_switch = PINB & _BV(PB3);
+                ignite_button = PINB & _BV(PB4);
+
+                if (key_switch && ignite_button) {
+                    // launch! set pin 0 high for 300ms
+                    PORTB |= _BV(PB0);
+                    _delay_ms(300);
+                    PORTB &= ~(_BV(PB0));
+                    // stop the loop to prevent triggering again
+                    break;
+                }
+                else {
+                    launch_armed = 0;
+                }
+            }
+
+            if (!key_switch) {
+                launch_armed = 0;
+            }
+        }
+        else {
+            // sleep until inputs change
+            sleep_enable();
+            sleep_bod_disable();
+            // enable interrupts
+            sei();
+            // actually sleep here
+            sleep_cpu();
+            sleep_disable();
+        }
     }
+
+    // TODO turn on reset LED
+    // require a power cycle of the controller to allow launching again
+    while (1) {}
 }
